b631df0a171972a85faebe85b0781bdfd9b4a260
Hiroto Okawa <okawahiroto@HirotonoMacBook-Pro.local>
2020-05-04 04:00:18 +0900

    20200504_scss・github-markdown.css追加

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // import "shitajicss/docs/css/shitajicss.min.css";
 import "../node_modules/shitajicss/dist/shitaji.min.css";
+import "./scss/style.scss";
 import Vue from 'vue'
 import App from './App.vue'
 
